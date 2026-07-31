0e9778b66313ae8e48351b83903564abe39ebfaa
arnold-i <a.ishimwe10@alustudent.com>
2026-07-31 05:35:29 -0700

    Task 3: to_json_string serializes an object

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Defines a to_json_string function."""
+import json
+
+
+def to_json_string(my_obj):
+    """Return the JSON string representation of an object.
+
+    Args:
+        my_obj: the object to serialize.
+    """
+    return json.dumps(my_obj)
