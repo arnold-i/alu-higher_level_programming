c06de2a7823d59c6f0c4755b2998e3de54106ece
arnold-i <a.ishimwe10@alustudent.com>
2026-07-31 06:03:12 -0700

    Task 1: create user_0d_1 with all privileges

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,3 @@
+-- Creates user user_0d_1 with all privileges (no failure if it exists).
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
